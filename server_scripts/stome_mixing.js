8f75b6fb028647a9f638800c6abefb0c75561026
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2025-10-22 17:31:02 +0100

    Refactor mixing recipes and add logging
    Removed old mixing recipes and added logging.

--- server_scripts/stome_mixing.js
+++ server_scripts/stome_mixing.js
@@ -1,15 +1,6 @@
 ServerEvents.recipes((e) => {
-	e.recipes.create.mixing(
-    	[Item.of("minecraft:netherrack", 2)],
-        [Item.of("minecraft:netherrack"), Item.of("create:cinder_flour")],
-    ).processingTime(100).heated();
-    
-    e.recipes.create.mixing(
-    	[Item.of("minecraft:blaze_powder", 2)],
-        [Item.of("minecraft:blaze_powder"), Ingredient.of("#c:coal")],
-    ).processingTime(500).heated();
 
-
+console.log('Adding mixing recipies');
     e.recipes.create.mixing(
     	[Item.of("create:asurine", 2)],
         [Item.of("create:asurine"), Item.of("minecraft:mossy_cobblestone")],
