dfab49bcd748cee72d9aea7a13169a1bb8780076
Niels Groen <140756283+hooded-person@users.noreply.github.com>
2025-10-27 18:48:03 +0100

    Add inventory removal

--- server_scripts/remove_diving_helmet.js
+++ server_scripts/remove_diving_helmet.js
@@ -1,4 +1,5 @@
-/* Preemptivly preventing crashes when submerging in water while wearing a diving helmet.
+// priority 100
+/* Preventing crashes when submerging in water while wearing a diving helmet.
 https://github.com/Creators-of-Create/Create/issues/8840
 https://github.com/Creators-of-Create/Create/issues/8817
 */
@@ -9,4 +10,28 @@ ServerEvents.recipes(event => {
     event.remove('create:netherite_diving_helmet');
 
 	console.log('Diving helmet recipes removed');
+});
+// Remove from inventory
+const bannedItems = [
+  "create:copper_diving_helmet",
+  "create:netherite_diving_helmet",
+];
+
+bannedItems.forEach((itemId) => {
+  PlayerEvents.inventoryChanged(itemId, (event) => {
+    const { player, item } = event;
+    if (itemId != item.id) {
+      console.log(
+        `Error: ${itemId} inventoryChanged handler called for item '${item.id}'. Does '${itemId}' exist?`
+      );
+      return;
+    }
+
+    if (player.isOp()) {
+      return;
+    }
+    player.inventory.clear(item);
+    player.tell(`${item.id} is a restricted item.`);
+    console.log(`Removed restricted item ${item.id} from ${player.getName()}`);
+  });
 });
